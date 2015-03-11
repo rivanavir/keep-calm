600ce4d47c8807f2db8a3414a58311a9dcd6ece1
Ivan <rivanyshka@gmail.com>
2015-03-11 16:38:04 +0200

    last version

--- js/main.js
+++ js/main.js
@@ -21,6 +21,23 @@ $(document).ready(function() {
 		});
 	});
 
+	$(function () {
+		//  Accordion Panels
+		$(".bot-inf-mob .title").click(function () {
+			$(this).next(".accord-for-mobile").slideToggle("slow").siblings(".accord-for-mobile:visible").slideUp("slow");
+			$(this).toggleClass("current");
+			$(this).siblings(".title").removeClass("current");
+		});
+	});
+
+	$(function () {
+		//  Accordion Panels
+		$(".mobile-comment").click(function () {
+			$(this).next(".mobile-comment-wrap").slideToggle("slow");
+			$(this).toggleClass("current");
+		});
+	});
+
 
 	// hide #back-top first
 	$("#back-top").hide();
@@ -77,11 +94,26 @@ $(document).ready(function() {
 		var currentLink = $(this).attr('href');
 		$('.section').removeClass('current').next('.pane').css('display','none');
 		$(currentLink).addClass('current').next('.pane').css('display','block');
+		if($( window ).width()<660){
+			var height = $('.section.current').offset().top-97;
+		} else{
+			var height = $('.section.current').offset().top-120;
+		}
+		$('html, body').animate({scrollTop:height}, '100');
 	});
 	$('.tabs-menu li a').on('click', function () {
 		$('.accordion .section').removeClass('current');
 		$('.accordion .pane').css('display','none');
 	});
+	$(".section").on('click',function () {
+		if (!$(this).hasClass('current')) {
+			console.log('current');
+			$(".keep-calm-ban").hide();
+			setTimeout (function(){
+				$(".keep-calm-ban").show();
+			}, 700);
+		};
+	})
 	$('.head-close').on('click', function () {
 		$(this).parent().css('display','none');
 	});
@@ -94,18 +126,42 @@ $(document).ready(function() {
 		e.preventDefault();
 		$(".product-layer").toggleClass('all-product');
 		if($(".product-layer").hasClass('all-product')){
-			// $(".product-layer").animate({height:"100%"}, 1500);
 			$(this).text("See less products");
 		} else{
-			// $(".product-layer").animate({height:"738px"}, 500);
 			$(this).text("See more products");
 		}
 	});
 
 	/*share-copy-wrap*/
-	$('#share-conn, #more-copy, #menu-button').on('click',function () {
+	$('.menu-button').on('click',function () {
 		$(this).toggleClass('active').next().toggleClass('dblock');
 	});
+	$('#more-copy').on('click',function () {
+		$(this).toggleClass('active').next().toggleClass('dblock');
+		$('#share-conn').removeClass('active').next().removeClass('dblock');
+	});
+	$('#share-conn').on('click',function () {
+		$(this).toggleClass('active').next().toggleClass('dblock');
+		$('#more-copy').removeClass('active').next().removeClass('dblock');
+	});
+		$('.notif').on('mouseenter',function () {
+			$(this).find('.count').addClass('count-hover');
+			$('#notif-list-wrap').show();
+		});
+		$('.notif').on('mouseleave',function () {
+			$(this).find('.count').removeClass('count-hover');
+			$('#notif-list-wrap').hide();
+		});
+		$('.menu-button').on('mouseenter',function () {
+			if($( window ).width()>1000){
+				$(this).addClass('active').find('.menu-list').addClass('dblock');
+			}
+		});
+		$('.menu-button').on('mouseleave',function () {
+			if($( window ).width()>1000){
+				$(this).removeClass('active').find('.menu-list').removeClass('dblock');
+			}
+		});
 	/*report-comment*/
 	$('.report-comment').on('mouseenter',function () {
 		$(this).append("<div class='rep-com-block'><p>Report Comment</p></div>");
@@ -127,13 +183,28 @@ $(document).ready(function() {
 	});
 
 	/*search form*/
-	$('#search-poster').on('focus',function () {
-		$('#poster-sf').addClass('focused');
-		$('#top-hit').addClass('dblock');
+		$('#search-poster').on('focus',function () {
+			if($( window ).width()>1000){
+				$('#poster-sf').addClass('focused');
+				$('#top-hit').addClass('dblock');
+			}
+		});
+		$('#search-poster').on('blur',function () {
+			if($( window ).width()>1000){
+				$('#poster-sf').removeClass('focused');
+				$('#top-hit').removeClass('dblock');
+			}
+		});
+
+	$('#tab-search-but').on('click',function () {
+		$(this).toggleClass('clicked');
+		$('.search-wrap').toggleClass('dblock');
+		$('#top-hit').toggleClass('dblock');
+		$('#poster-sf').toggleClass('focused').find('input').focus();
 	});
-	$('#search-poster').on('blur',function () {
-		$('#poster-sf').removeClass('focused');
-		$('#top-hit').removeClass('dblock');
+	$('#mob-search-but').on('click',function () {
+		$(this).toggleClass('clicked');
+		$('.mob-search-form').slideToggle('slow').find('#mob-search').focus();
 	});
 
 	/*popup*/
@@ -159,16 +230,50 @@ $(document).ready(function() {
 	});
 
 
-	$('.scroll-wrapper').jScrollPane({
-		verticalGutter:5,
-		mouseWheelSpeed:15,
-		'showArrows': false
+	$('.notif').hover(function () {
+		$('.scroll-wrapper').jScrollPane({
+			verticalGutter:5,
+			mouseWheelSpeed:15,
+			'showArrows': false
+		});
+		$('.jspDrag').hide();
+		$('.jspScrollable').mouseenter(function(){
+			$(this).find('.jspDrag').stop(true, true).fadeIn('slow');
+		});
+		$('.jspScrollable').mouseleave(function(){
+			$(this).find('.jspDrag').stop(true, true).fadeOut('slow');
+		});
 	});
-	$('.jspDrag').hide();
-	$('.jspScrollable').mouseenter(function(){
-		$(this).find('.jspDrag').stop(true, true).fadeIn('slow');
-	});
-	$('.jspScrollable').mouseleave(function(){
-		$(this).find('.jspDrag').stop(true, true).fadeOut('slow');
-	});
+
+	/*MOBILE*/
+	$('#profile-mob img').on('click',function () {
+		if($( window ).width()<1000){
+			$('.profile-list').toggleClass('visible');
+		}
+	})
+	$('#notif-button').on('click',function () {
+		if($( window ).width()<1000){
+			$('#notif-list-wrap').toggleClass('visible');
+		}
+	})
+
+	if($( window ).width()<660){
+		var accord = $(".bottom-info-layer >*").clone();
+		$(".bot-inf-mob").append(accord);
+	} else{
+		$(".bot-inf-mob").remove();
+	}
+
+	setInterval( function() {
+		// if($( window ).width()>660){
+			var headHeight = $("header").height();
+			$(".mobile-wrap").css('padding-top',headHeight);
+		// }
+	} , 100);
+
+	$('.poster-action .lk, .poster-action .st').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('pa-active');
+	})
+
 });
